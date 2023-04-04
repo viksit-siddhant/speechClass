d100fa676be2134871a046616cb65e54a0078847
viksit-siddhant <s.r.viksit@gmail.com>
2023-04-04 20:15:49 +0530

    Fixed feature extraction in deepSpectrum

--- models/deepSpectrum.py
+++ models/deepSpectrum.py
@@ -8,7 +8,7 @@ class Model:
         if torch.cuda.is_available():
             self.model = self.model.cuda()
         self.features = []
-        self.model.fc7.register_forward_hook(self.get_features('fc7'))
+        [l for l in self.model.children()][-3].register_forward_hook(self.get_features('fc7'))
 
         for layer in self.model.children():
             for param in layer.parameters():
